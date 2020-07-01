3b874527646b389450a40a7eac46a7d1a53e1220
Seth <sethmaxwl@gmail.com>
2020-07-01 02:39:17 -0400

    Fix linting errors

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -5,13 +5,13 @@
 </template>
 
 <script>
-import Splash from '@/components/landing-components/Splash'
-import About from '@/components/landing-components/About'
+import Splash from '@/components/landing-components/Splash';
+import About from '@/components/landing-components/About';
 export default {
   name: 'Landing',
   components: {
     Splash,
-    About
-  }
-}
+    About,
+  },
+};
 </script>
--- src/mixins/utility.js
+++ src/mixins/utility.js
@@ -1,11 +1,11 @@
-import router from '@/router'
+import router from '@/router';
 
 export const utility = {
   methods: {
     redirect(route) {
       if (this.$route.fullPath !== route) {
-        router.push(route)
+        router.push(route);
       }
-    }
-  }
-}
+    },
+  },
+};
